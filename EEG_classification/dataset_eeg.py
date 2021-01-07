24d1b869f926100926c7a93dcb99cc64a99b6c8c
Jin <haya.jinlee@gmail.com>
2021-01-07 13:53:47 +0100

    Implement train valid split and simple loss return

--- EEG_classification/dataset_eeg.py
+++ EEG_classification/dataset_eeg.py
@@ -4,25 +4,46 @@ import numpy as np
 import glob
 import scipy.io
 
-class EmotionEEG(Dataset):
-  def __init__(self, datapath):
-    self.files = glob.glob(datapath + '*')
-    self.load()
-  def load(self):
-    self.EEG=torch.empty(1280,32,8064)
-    self.label = torch.empty(1280,4)
-    trial_count = 0
-    for f in self.files:
-      data=scipy.io.loadmat(f)
-      eeg=torch.from_numpy(data['data'][:,:32,:])
-      label=torch.from_numpy(data['labels'])
-      self.EEG[trial_count:trial_count+eeg.shape[0]]=eeg
-      self.label[trial_count:trial_count+eeg.shape[0]]=label
-      trial_count+=eeg.shape[0]
-      if trial_count >=100:
-        break
 
-  def __len__(self):
-    return self.data.shape[0]
-  def __getitem__(self, index):
-    return {'eeg':self.EEG[index], 'label':self.label[index]}
+class EmotionEEG(Dataset):
+    def __init__(self, datapath, seg_length=640):
+        self.files = glob.glob(datapath + "*")
+        self.seg_length = seg_length  ##640
+        self.load()
+
+    def load(self):
+        num_session = len(self.files)
+        trial_per_session = 40
+        seg_per_session = 8064 // self.seg_length  ## 12
+        self.EEG = torch.empty(
+            seg_per_session * trial_per_session * num_session, 32, seg_length
+        )
+        self.label = torch.empty(seg_per_session * trial_per_session * num_session, 4)
+
+        trial_count = 0
+        for f in self.files:
+            data = scipy.io.loadmat(f)
+            eeg = data["data"][:, :32, : seg_length * seg_per_session]
+            split_eeg = torch.from_numpy(
+                np.array(np.split(eeg, 12, axis=-1)).reshape(-1, 32, seg_length)
+            )
+            split_label = torch.from_numpy(
+                np.tile(data["labels"], (seg_per_session, 1))
+            )
+            self.EEG[trial_count : trial_count + split_eeg.shape[0]] = split_eeg
+            self.label[trial_count : trial_count + split_eeg.shape[0]] = split_label
+            trial_count += split_eeg.shape[0]
+
+    def __len__(self):
+        return self.EEG.shape[0]
+
+    def __getitem__(self, index):
+        return {"eeg": self.EEG[index], "label": self.label[index]}
+
+    def train_valid_split(self, split_ratio=0.2, seed=0):
+        np.random.seed(seed)
+        split_idx = int(split_ratio * len(self))
+        indices = np.arange(len(self))
+        np.random.shuffle(indices)
+        train_indices, val_indices = indices[split_idx:], indices[:split_idx]
+        return train_indices, val_indices
--- EEG_classification/train.py
+++ EEG_classification/train.py
@@ -2,6 +2,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from classifiers import EmotionLSTM
 from dataset_eeg import EmotionEEG
+import time
 ## For LSTM
 
 def label_to_class(labels, state, num_class):
@@ -13,27 +14,50 @@ def label_to_class(labels, state, num_class):
         cl=l>=0.5
         ## TODO: implment 3 classes case
 
-    return cl.long()
+    return cl.float()
 
 def train(args):
     
     
     dataset=EmotionEEG(args.datapath)
-    dataloader=DataLoader(dataset=dataset, batch_size = args.batch_size, shuffle=True)
+    train_indices, val_indices = dataset.train_valid_split(split_ratio=args.split_ratio, seed=args.seed)
+    train_sampler = SubsetRandomSampler(train_indices)
+    valid_sampler = SubsetRandomSampler(val_indices)
+    train_dataloader=DataLoader(dataset=dataset, args.batch_size =64, sampler=train_sampler)
+    valid_dataloader=DataLoader(dataset=dataset, args.batch_size =64, sampler=train_sampler)
     emotion=EmotionLSTM(args.num_class)
-    criterion = nn.CrossEntropyLoss()
+    criterion = nn.BCELoss()
     optimizer = optim.Adam(emotion.parameters())
 
-    for epoch in range(args.epochs):
-        for i, batch in enumerate(dataloader):
+    train_losses=[]
+    val_accs =[]
+    
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    for epoch in range(30):
+        for i, batch in enumerate(train_dataloader):
             eeg=batch['eeg'].transpose(1,-1)
-            cl=label_to_class(batch['label'], args.state, args.num_class)
+            cl=label_to_class(batch['label'][:,0], 'arousal')
             optimizer.zero_grad()
-            pred = emotion(eeg).squeeze()
+            pred = emotion(eeg).squeeze().mean(dim=1)
             loss = criterion(pred, cl)
             loss.backward()
             optimizer.step()
-            ##TODO - Implement checkpoints, logs
+            if i%5==1:
+                train_losses.append(loss)
+                print(f'Train loss: {loss}')
+        val_accs_running=[]
+        for i, batch in enumerate(valid_dataloader):
+            pred = emotion(eeg).squeeze().mean(dim=1) > 0.5
+            cl=label_to_class(batch['label'][:,0], 'arousal') ==1
+            acc=(pred == cl).sum()/len(cl)
+            val_accs_running.append(acc)
+            
+        val_accs.append(np.mean(val_accs_running))
+        print(f'Valid acc: {np.mean(val_accs_running)}')
+
+    torch.save(emotion.state_dict(), args.checkpoint+'/eeglstm_'+ timestr +'.pt')
+
+    return train_losses, val_accs
 
 
 if __name__ == "__main__":
@@ -42,5 +66,8 @@ if __name__ == "__main__":
     parser.add_argument('--batch-size', type=int, help='batch size of data loader')
     parser.add_argument('--num-class', type=int, help='2 classes or 3 classes problem')
     parser.add_argument('state', type=str), help='state to classify')
+    parser.add_argument('seed', type=int, help='train, valid split seed')
+    parser.add_argument('--split-ratio', type=float, help='train, valid split ratio')
+    parser.add_argument('checkpoint', type=str, hep='dir to save model')
     args = parser.parse_args()
     train(args)
